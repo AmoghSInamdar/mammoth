b0afadea7796e376d60d13294325268ebfbe68a4
Amogh   Inamdar <amoghinamdar@gmail.com>
2026-04-20 21:36:14 -0400

    update adapt lr

--- k_shot_pipeline_commands.sh
+++ k_shot_pipeline_commands.sh
@@ -235,45 +235,45 @@ CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6 \
 
 CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 \
     python run_pipeline_full.py --dataset smooth-rot-mnist --model sgd  --lr 0.1 \
-    --adapt_lr 0.2 --num_adapt_steps 10 \
+    --adapt_lr 0.003 --num_adapt_steps 10 \
     --savecheck task \
     >outputs/sgd_smooth_mnist.out 2>outputs/sgd_smooth_mnist.err &
 
 CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6 \
     python run_pipeline_full.py --dataset smooth-rot-mnist --model er --buffer_size 500 --lr 0.1 \
-    --adapt_lr 0.2 --num_adapt_steps 10 \
+    --adapt_lr 0.003 --num_adapt_steps 10 \
     --savecheck task \
     >outputs/er_smooth_mnist.out 2>outputs/er_smooth_mnist.err &
 
 CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6 \
     python run_pipeline_full.py --dataset smooth-rot-mnist --model derpp --lr 0.03 --buffer_size 500 --alpha 0.3 --beta 0.5 \
-    --adapt_lr 0.2 --num_adapt_steps 10 \
+    --adapt_lr 0.003 --num_adapt_steps 10 \
     --savecheck task \
     >outputs/derpp_smooth_mnist.out 2>outputs/derpp_smooth_mnist.err &
 
 CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6 \
     python run_pipeline_full.py --dataset smooth-rot-mnist --model ewc_on --lr 0.1 --e_lambda 10 --gamma 1 \
-    --adapt_lr 0.2 --num_adapt_steps 10 \
+    --adapt_lr 0.003 --num_adapt_steps 10 \
     --savecheck task \
     >outputs/ewc_smooth_mnist.out 2>outputs/ewc_smooth_mnist.err &
 
 CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6 \
     python run_pipeline_full.py --dataset smooth-rot-mnist --model agem --buffer_size 500 --lr 0.03 \
-    --adapt_lr 0.2 --num_adapt_steps 10 \
+    --adapt_lr 0.003 --num_adapt_steps 10 \
     --savecheck task \
     --skip_train \
     >outputs/agem_smooth_mnist.out 2>outputs/agem_smooth_mnist.err &
 
 CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6 \
     python run_pipeline_full.py --dataset smooth-rot-mnist --model lwf --lr 0.03 \
-    --adapt_lr 0.2 --num_adapt_steps 10 \
+    --adapt_lr 0.003 --num_adapt_steps 10 \
     --savecheck task \
     --skip_train \
     >outputs/lwf_smooth_mnist.out 2>outputs/lwf_smooth_mnist.err &
 
 CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 \
     python run_pipeline_full.py --dataset smooth-rot-mnist --model mer --lr 0.1 --beta 0.01 --gamma 0.03 --buffer_size 200 --minibatch_size 25 --n_epochs 1 \
-    --adapt_lr 0.2 --num_adapt_steps 10 \
+    --adapt_lr 0.003 --num_adapt_steps 10 \
     --savecheck task \
     >outputs/mer_smooth_mnist.out 2>outputs/mer_smooth_mnist.err &
 
@@ -283,7 +283,7 @@ CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 \
     python run_pipeline_full.py --dataset smooth-rot-mnist --model meta_sgd  --lr 0.1 \
     --meta_method reptile --meta_strategy parallel --num_lookahead_tasks 3 --meta_lr 0.01 \
     --meta_adapt_lr 0.2 --meta_adapt_steps 10 --num_meta_examples 25 \
-    --adapt_lr 0.2 --num_adapt_steps 10 \
+    --adapt_lr 0.003 --num_adapt_steps 10 \
     --savecheck task \
     >outputs/meta_sgd_smooth_mnist.out 2>outputs/meta_sgd_smooth_mnist.err &
 
@@ -291,7 +291,7 @@ CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 \
     python run_pipeline_full.py --dataset smooth-rot-mnist --model meta_sgd  --lr 0.1 \
     --meta_method no_meta --meta_strategy parallel --num_lookahead_tasks 3 --meta_lr 0.1 \
     --meta_adapt_lr 0.2 --meta_adapt_steps 10 --num_meta_examples 25 \
-    --adapt_lr 0.2 --num_adapt_steps 10 \
+    --adapt_lr 0.003 --num_adapt_steps 10 \
     --savecheck task \
     >outputs/no_meta_sgd_smooth_mnist.out 2>outputs/no_meta_sgd_smooth_mnist.err &
 
@@ -299,6 +299,6 @@ CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 \
     python run_pipeline_full.py --dataset smooth-rot-mnist --model meta_sgd  --lr 0.1 \
     --meta_method maml --meta_strategy parallel --num_lookahead_tasks 3 --meta_lr 0.001 \
     --meta_adapt_lr 0.2 --meta_adapt_steps 10 --num_meta_examples 25 \
-    --adapt_lr 0.2 --num_adapt_steps 10 \
+    --adapt_lr 0.003 --num_adapt_steps 10 \
     --savecheck task \
     >outputs/maml_sgd_smooth_mnist.out 2>outputs/maml_sgd_smooth_mnist.err &
